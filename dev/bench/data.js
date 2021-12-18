35d61a11051b7d78f3772c19d07d984a9f843757
github-action-benchmark <github@users.noreply.github.com>
2021-12-18 14:36:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c49610f51b46c199ffb91337796fd06baa2534c3

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1639838200029,
+  "repoUrl": "https://github.com/Syu-fu/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "syu.fu.dev@gmail.com",
+            "name": "Syu fu",
+            "username": "Syu-fu"
+          },
+          "committer": {
+            "email": "syu.fu.dev@gmail.com",
+            "name": "Syu fu",
+            "username": "Syu-fu"
+          },
+          "distinct": true,
+          "id": "c49610f51b46c199ffb91337796fd06baa2534c3",
+          "message": "Initial commit",
+          "timestamp": "2021-12-18T23:32:09+09:00",
+          "tree_id": "821523decd9e3db070a65c0abb94b48f70f0ea68",
+          "url": "https://github.com/Syu-fu/dotfiles/commit/c49610f51b46c199ffb91337796fd06baa2534c3"
+        },
+        "date": 1639838199830,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 0.06,
+            "unit": "Second"
+          },
+          {
+            "name": "neovim load time",
+            "value": 0.0904,
+            "unit": "Second"
+          }
+        ]
+      }
+    ]
+  }
+}
