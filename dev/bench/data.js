347229a2192379b17bdcbaa2bbc465f2c4877c94
github-action-benchmark <github@users.noreply.github.com>
2021-12-21 12:28:24 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e1917a4f6f622cda33009602b83a5b73f1684ecb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639838200029,
+  "lastUpdate": 1640089704784,
   "repoUrl": "https://github.com/Syu-fu/dotfiles",
   "entries": {
     "Benchmark": [
@@ -33,6 +33,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "neovim load time",
             "value": 0.0904,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "syu.fu.dev@gmail.com",
+            "name": "Syu fu",
+            "username": "Syu-fu"
+          },
+          "committer": {
+            "email": "syu.fu.dev@gmail.com",
+            "name": "Syu fu",
+            "username": "Syu-fu"
+          },
+          "distinct": true,
+          "id": "e1917a4f6f622cda33009602b83a5b73f1684ecb",
+          "message": "[vim]fzf setting fix",
+          "timestamp": "2021-12-21T21:11:39+09:00",
+          "tree_id": "fc79bf1c4bdf822f43856e8a1757236dac094bac",
+          "url": "https://github.com/Syu-fu/dotfiles/commit/e1917a4f6f622cda33009602b83a5b73f1684ecb"
+        },
+        "date": 1640089704446,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 0.08,
+            "unit": "Second"
+          },
+          {
+            "name": "neovim load time",
+            "value": 0.1268,
             "unit": "Second"
           }
         ]
