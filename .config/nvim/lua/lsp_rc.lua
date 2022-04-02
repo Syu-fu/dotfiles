9ea78f6b52438ad7ef33257954b998390e87eb2a
Syu fu <syu.fu.dev@gmail.com>
2022-04-03 03:32:07 +0900

    [vim] Set LSP

--- .config/nvim/lua/lsp_rc.lua
+++ .config/nvim/lua/lsp_rc.lua
@@ -1,47 +1,62 @@
-local on_attach = function(client)
-  vim.wo.signcolumn = 'yes'
-  local maps = {
-    {'n', '<c-]>',     '<cmd>lua vim.lsp.buf.definition()<CR>'},
-    {'n', 'K',         '<cmd>lua vim.lsp.buf.hover()<CR>'},
-    {'n', 'gD',        '<cmd>lua vim.lsp.buf.implementation()<CR>'},
-    {'n', '1gD',       '<cmd>lua vim.lsp.buf.type_definition()<CR>'},
-    {'n', 'gW',        '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>'},
-    {'n', 'gd',        '<cmd>lua vim.lsp.buf.declaration()<CR>'},
-    {'n', '<Leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>'},
-    {'n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>'},
-    {'n', 'gl',        '<cmd>lua vim.lsp.buf.document_highlight()<CR>'},
-    {'n', 'gm',        '<cmd>lua vim.diagnostic.open_float()<CR>'},
-    {'n', 'g0',        '<cmd>Denite lsp/document_symbol -auto-action=highlight<CR>'},
-    {'n', 'gr',        '<cmd>Denite lsp/references -auto-action=preview_bat<CR>'},
-  }
-  for _, map in ipairs(maps) do
-    vim.api.nvim_buf_set_keymap(0, map[1], map[2], map[3], {noremap = true})
-  end
-
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = false,
-    }
-  )
-  -- require "lsp_signature".on_attach({
-  --   floating_window = false,
-  -- })  -- Note: add in lsp client on-attach
-
-  vim.api.nvim_exec(
-  [[
-    augroup MyLspSettings
-      autocmd!
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-  ]],
-  false)
+-- To instead override globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+require 'lspconfig'.myservertwo.setup {}
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+end
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+    opts.on_attach = on_attach
+
+    server:setup(opts)
+end)
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.preselectSupport = true
 capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
 capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.deprecatedSupport = true
 capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
 capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
@@ -52,48 +67,5 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
     'additionalTextEdits',
   }
 }
-
-require'lspconfig'.clangd.setup{on_attach = on_attach, capabilities = capabilities}
-require'lspconfig'.pylsp.setup{on_attach = on_attach, capabilities = capabilities}
-require'lspconfig'.rls.setup{on_attach = on_attach, capabilities = capabilities}
--- require'lspconfig'.texlab.setup{on_attach = on_attach, capabilities = capabilities}
-require'lspconfig'.gopls.setup{on_attach = on_attach, capabilities = capabilities}
-require'lspconfig'.denols.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = {
-    lint = true,
-    unstable = true,
-  },
-}
-
-local lsp_installer = require("nvim-lsp-installer")
-
-lsp_installer.on_server_ready(function(server)
-  local opts = {}
-
-  if server.name == 'sumneko_lua' then
-    opts.settings = {
-      Lua = {
-        runtime = {
-          version = 'LuaJIT',
-          path = vim.split(package.path, ';'),
-        },
-        diagnostics = {
-          globals = {'vim'},
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-          },
-        },
-      },
-    }
-  end
-
-  opts.on_attach = on_attach
-  opts.capabilities = capabilities
-  server:setup(opts)
-end)
+nvim_lsp.tsserver.setup{on_attach = on_attach, capabilities = capabilities}
 
