669e522d707cff3562ba270ccea3a0c24d9f07fe
Syu fu <syu.fu.dev@gmail.com>
2022-04-18 22:01:38 +0900

    [zsh] powerlevel10k from 256colors to true color

--- .p10k.zsh
+++ .p10k.zsh
@@ -178,8 +178,10 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=223
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=208
+  #typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=223#d4be98
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=#d4be98
+  #typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=208
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=#34be5b
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
@@ -208,19 +210,20 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=237
+  #typeset -g POWERLEVEL9K_DIR_BACKGROUND=237
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=#3c3837
   # Default current directory foreground color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=142
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=#a9b665
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=142
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=#a9b665
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=142
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=#a9b665
   # Display anchor directory segments in bold.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
   # Don't shorten directories that contain any of these files. They are anchors.
@@ -344,8 +347,8 @@
   # typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
   # typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
   # typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
-  typeset -g POWERLEVEL9K_VCS_BACKGROUND=240
-  typeset -g POWERLEVEL9K_VCS_FOREGROUND=109
+  typeset -g POWERLEVEL9K_VCS_BACKGROUND=#504945
+  typeset -g POWERLEVEL9K_VCS_FOREGROUND=#7daea3
 
   # Branch icon. Set this parameter to '\uF126 ' for the popular Powerline branch icon.
   typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
@@ -502,8 +505,8 @@
   # Use terse signal names: "INT" instead of "SIGINT(2)".
   typeset -g POWERLEVEL9K_STATUS_VERBOSE_SIGNAME=false
   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION='✘'
-   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND=235
-   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_BACKGROUND=167
+   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND=#282828
+   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_BACKGROUND=#ea6962
 
   # Status when some part of a pipe command fails and the overall exit status is also non-zero.
   # It may look like this: 1|0.
@@ -515,7 +518,7 @@
   ###################[ command_execution_time: duration of the last command ]###################
   # Execution time color.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=0
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_BACKGROUND=175
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_BACKGROUND=#d3869b
   # Show duration of the last command if takes at least this many seconds.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
   # Show this many fractional digits. Zero means round to seconds.
