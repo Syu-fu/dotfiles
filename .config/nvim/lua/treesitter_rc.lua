1d70c74299c8edec29a25aca4d7753b41d4966b9
Syu fu <syu.fu.dev@gmail.com>
2022-05-04 17:41:00 +0900

    [vim] Treesitter install indivisual designation

--- .config/nvim/lua/treesitter_rc.lua
+++ .config/nvim/lua/treesitter_rc.lua
@@ -1,9 +1,26 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
         disable = {
             'toml',
         }
     },
-    ensure_installed = 'maintained'
+    ensure_installed = {
+        'bash',
+        'css',
+        'dockerfile',
+        'go',
+        'gomod',
+        'graphql',
+        'html',
+        'javascript',
+        'jsdoc',
+        'json',
+        'json5',
+        'JSON with Comment',
+        'typescript',
+        'tsx',
+        'yaml',
+        'vim'
+    }
 }
