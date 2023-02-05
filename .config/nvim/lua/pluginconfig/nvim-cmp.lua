e5e03be4d7193d9b8cb335c6940c73a9f3c03c01
Syu fu <syu.fu.dev@gmail.com>
2023-02-05 21:13:01 +0900

    feat(vim): Update nvim-cmp settings

--- .config/nvim/lua/pluginconfig/nvim-cmp.lua
+++ .config/nvim/lua/pluginconfig/nvim-cmp.lua
@@ -1,20 +1,10 @@
-vim.g.completeopt = 'menu,menuone,noselect'
-
 local cmp = require('cmp')
 local types = require('cmp.types')
 local luasnip = require('luasnip')
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
+local dict = require('cmp_dictionary')
 
 cmp.setup({
   formatting = {
-    -- fields = {'abbr', 'kind', 'menu'},
     format = require('lspkind').cmp_format({
       with_text = true,
       menu = {
@@ -23,11 +13,7 @@ cmp.setup({
         luasnip = '[LuaSnip]',
         nvim_lua = '[NeovimLua]',
         path = '[Path]',
-        spell = '[Spell]',
         emoji = '[Emoji]',
-        calc = '[Calc]',
-        rg = '[Rg]',
-        treesitter = '[TS]',
         dictionary = '[Dictionary]',
         cmdline_history = '[History]',
       },
@@ -35,7 +21,7 @@ cmp.setup({
   },
   snippet = {
     expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      luasnip.lsp_expand(args.body)
     end,
   },
   sorting = {
@@ -70,115 +56,83 @@ cmp.setup({
   },
 
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<Up>'] = cmp.mapping(function(_)
-      if cmp.visible() then
-        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
-      end
-    end, { 'i' }),
-    ['<Down>'] = cmp.mapping(function(_)
-      if cmp.visible() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
-      end
-    end, { 'i' }),
-
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-        -- elseif luasnip.expand_or_jumpable() then
-        -- 	luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-
-    ['<C-Down>'] = cmp.mapping(function(fallback)
-      if luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-
-    ['<C-Up>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-q>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
-  -- LuaFormatter off
+
   sources = cmp.config.sources({
-    -- { name = "copilot", priority = 90 }, -- For luasnip users.
     { name = 'nvim_lsp', priority = 100 },
-    { name = 'cmp_tabnine', priority = 30 },
-    { name = 'luasnip', priority = 20 }, -- For luasnip users.
+    { name = 'luasnip', priority = 20 },
     { name = 'path', priority = 100 },
     { name = 'emoji', insert = true, priority = 60 },
     { name = 'nvim_lua', priority = 50 },
     { name = 'nvim_lsp_signature_help', priority = 80 },
   }, {
     { name = 'buffer', priority = 50 },
-    -- slow
-    -- { name = "omni", priority = 40 },
-    { name = 'spell', priority = 40 },
-    { name = 'calc', priority = 50 },
-    { name = 'treesitter', priority = 30 },
     { name = 'dictionary', keyword_length = 2, priority = 10 },
   }),
-  -- LuaFormatter on
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' },
+  }, {
+    { name = 'buffer' },
+  }),
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  mapping = {
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { 'c' }),
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
 
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { 'c' }),
-    ['<C-y>'] = {
-      c = cmp.mapping.confirm({ select = false }),
-    },
-    ['<C-q>'] = {
-      c = cmp.mapping.abort(),
-    },
-  },
   sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' }, { { name = 'cmdline_history' } } }),
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp_document_symbol' },
+  }, {
+    { name = 'buffer' },
+  }),
 })
 
 -- autopairs
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+-- dictionary
+dict.setup({
+  dic = {
+    ['*'] = { '/usr/share/dict/words' },
+  },
+  exact = 2,
+  first_case_insensitive = false,
+  document = false,
+  document_command = 'wn %s -over',
+  max_items = -1,
+})
+
+dict.switcher({
+  spelllang = {
+    en = '/usr/share/dict/words',
+  },
+})
