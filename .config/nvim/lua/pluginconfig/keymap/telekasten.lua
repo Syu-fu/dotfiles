08d749830aec17cc14534bb719ee5b1501745ac1
Syu fu <syu.fu.dev@gmail.com>
2023-07-08 17:34:58 +0900

    feat(vim): Remove telekasten configuration

--- .config/nvim/lua/pluginconfig/keymap/telekasten.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<Space>mn', '<Cmd>Telekasten new_templated_note<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Space>ml', '<Cmd>Telekasten find_notes<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Space>mg', '<Cmd>Telekasten search_notes<CR>', { noremap = true, silent = true })
