3f73ed63b541ab71b68cd049dc8ee411e7736e27
Syu fu <syu.fu.dev@gmail.com>
2023-08-16 18:50:20 +0900

    feat(vim): Add fcitx settings for Neovim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -48,3 +48,18 @@ vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap =
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_create_augroup('ime_ctrl', { clear = true })
+vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
+  group = 'ime_ctrl',
+  pattern = '*',
+  callback = function()
+    if vim.fn.has('linux') == 1 then
+      local input_status = vim.fn.system('fcitx-remote')
+      if input_status == '2\n' then
+        vim.fn.system('fcitx-remote -c')
+      end
+    end
+  end,
+  once = false,
+})
