f2bdcdcf7d7e39d68d0403414022388f453118c7
Syu fu <syu.fu.dev@gmail.com>
2023-10-05 22:50:26 +0900

    feat: Add completion

--- .config/zsh/rc/completion.zsh
+++ .config/zsh/rc/completion.zsh
@@ -1,5 +1,8 @@
 if command -v aqua &> /dev/null; then source <(aqua completion zsh); fi
+if command -v hugo &> /dev/null; then source <(hugo completion zsh); fi
+if command -v deno &> /dev/null; then source <(deno completions zsh); fi
 if command -v golangci-lint &> /dev/null; then source <(golangci-lint completion zsh); fi
+if command -v yq &> /dev/null; then (yq shell-completion zsh)> $ZDOTDIR/completions/_yq; fi
 if command -v gh &> /dev/null; then (gh completion --shell zsh)> $ZDOTDIR/completions/_gh; fi
 if command -v fd &> /dev/null; then (fd --gen-completions)> $ZDOTDIR/completions/_fd; fi
 if command -v rg &> /dev/null && [ ! -e $ZDOTDIR/completions/_rg ]; then
