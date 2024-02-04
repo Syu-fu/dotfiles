fc977bf7c3eacd0b703d117294bdd86c3937fcfb
Syu fu <syu.fu.dev@gmail.com>
2024-02-04 16:33:03 +0900

    fix: Fix ddc lsp settings

--- home/.config/nvim/lua/pluginconfig/mason-lspconfig.lua
+++ home/.config/nvim/lua/pluginconfig/mason-lspconfig.lua
@@ -1,6 +1,6 @@
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   local function buf_set_keymap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
@@ -37,7 +37,37 @@ local on_attach = function(client, bufnr)
 end
 
 local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+---@param names string[]
+---@return string[]
+local function get_plugin_paths(names)
+  local plugins = require('lazy.core.config').plugins
+  local paths = {}
+  for _, name in ipairs(names) do
+    if plugins[name] then
+      table.insert(paths, plugins[name].dir .. '/lua')
+    else
+      vim.notify('Invalid plugin name: ' .. name)
+    end
+  end
+  return paths
+end
+
+---@param plugins string[]
+---@return string[]
+local function library(plugins)
+  local paths = get_plugin_paths(plugins)
+  table.insert(paths, vim.fn.stdpath('config') .. '/lua')
+  table.insert(paths, vim.env.VIMRUNTIME .. '/lua')
+  table.insert(paths, '${3rd}/luv/library')
+  table.insert(paths, '${3rd}/busted/library')
+  table.insert(paths, '${3rd}/luassert/library')
+  return paths
+end
+
+--  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require('ddc_source_lsp').make_client_capabilities()
+
 require('mason-lspconfig').setup_handlers({
   function(server_name)
     lspconfig[server_name].setup({ capabilities = capabilities, on_attach = on_attach })
@@ -50,6 +80,15 @@ require('mason-lspconfig').setup_handlers({
         Lua = {
           diagnostics = {
             globals = { 'vim' },
+          },
+          runtime = {
+            version = 'LuaJIT',
+            pathStrict = true,
+            path = { '?.lua', '?/init.lua' },
+          },
+          workspace = {
+            library = library({ 'lazy.nvim' }),
+            checkThirdParty = 'Disable',
           },
         },
       },
