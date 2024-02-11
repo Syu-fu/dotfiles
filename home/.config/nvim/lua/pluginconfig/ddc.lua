a850e460675d4e320286ce6ec221b7b665b9ee52
Syu fu <syu.fu.dev@gmail.com>
2024-02-11 10:25:46 +0900

    fix: Fix ddc config

--- home/.config/nvim/lua/pluginconfig/ddc.lua
+++ home/.config/nvim/lua/pluginconfig/ddc.lua
@@ -10,6 +10,8 @@ vim.fn['ddc#custom#patch_global']({
   sources = {
     'lsp',
     'dictionary',
+    'buffer',
+    'path',
   },
   sourceOptions = {
     _ = {
@@ -29,21 +31,40 @@ vim.fn['ddc#custom#patch_global']({
     },
     lsp = {
       mark = '[LSP]',
-      --dup = 'keep',
       keywordPattern = '\\k+',
-      matchers = {
-        'matcher_fuzzy',
-      },
-      sorters = {
-        'sorter_fuzzy',
-      },
       converters = {
         'converter_kind_labels',
       },
     },
+    buffer = {
+      mark = '[Buffer]',
+    },
+    around = {
+      mark = '[Around]',
+    },
+    line = {
+      mark = '[Line]',
+    },
+    path = {
+      mark = '[Path]',
+    },
     dictionary = {
       mark = '[Dict]',
-      matchers = { 'matcher_fuzzy' },
+    },
+    ['git-commit'] = {
+      mark = '[Git]',
+    },
+    ['git-file'] = {
+      mark = '[Git]',
+    },
+    ['git-branch'] = {
+      mark = '[Git]',
+    },
+    github_issue = {
+      mark = '[GitHub]',
+    },
+    github_pull_request = {
+      mark = '[GitHub]',
     },
   },
   sourceParams = {
@@ -105,6 +126,30 @@ vim.fn['ddc#custom#patch_global']({
   },
 })
 
+vim.fn['ddc#custom#patch_global']({
+  sourceOptions = {
+    cmdline = {
+      mark = '[Cmd]',
+      keywordPattern = '[\\w#:~_-]*',
+      matchers = { 'matcher_fuzzy' },
+      sorters = { 'sorter_fuzzy' },
+      converters = { 'converter_cmdline' },
+    },
+    ['cmdline-history'] = { mark = '[Hist]' },
+  },
+  cmdlineSources = {
+    [':'] = { 'cmdline', 'cmdline-history', 'around', 'git-commit', 'git-file', 'git-branch' },
+    ['/'] = { 'around', 'buffer', 'line' },
+    ['?'] = { 'cmdline', 'cmdline-history' },
+    ['@'] = { 'cmdline-history', 'buffer' },
+    ['='] = { 'input' },
+  },
+})
+
+vim.fn['ddc#custom#patch_filetype']({ 'gitcommit' }, {
+  sources = { 'github_issue', 'github_pullrequest', 'git-commit', 'dictionary' },
+})
+
 vim.fn['pum#set_option']({
   item_orders = { 'kind', 'space', 'abbr', 'space', 'menu' },
   scrollbar_char = '┃',
@@ -120,6 +165,20 @@ vim.api.nvim_create_autocmd('BufEnter', {
     end)
   end,
 })
+
+-- gh buffer
+function OnGh()
+  local tmpdir = vim.fn.empty(vim.env.TMPDIR) and '/tmp' or vim.env.TMPDIR
+  if vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':h') ~= tmpdir or vim.fn.getcwd() == tmpdir then
+    return
+  end
+  vim.fn['ddc#custom#patch_buffer']({
+    sources = { 'github_issue', 'github_pull_request' },
+  })
+end
+
+vim.api.nvim_create_autocmd('FileType', { pattern = 'markdown', callback = OnGh })
+
 --pum
 vim.keymap.set({ 'i', 'c' }, '<Tab>', function()
   if vim.fn['pum#visible']() then
@@ -160,6 +219,18 @@ vim.keymap.set({ 'i', 'c' }, '<c-c>', function()
   end
   return '<c-c>'
 end, { expr = true })
+-- cmdline keymap
+vim.keymap.set('c', '<Tab>', function()
+  if vim.fn['ddc#visible']() then
+    vim.fn['pum#map#insert_relative'](1, 'loop')
+  else
+    return vim.fn['ddc#map#manual_complete']()
+  end
+end, { expr = true, replace_keycodes = false })
+vim.keymap.set('c', '<S-Tab>', "<Cmd>call pum#map#insert_relative(-1, 'loop')<CR>")
+vim.keymap.set('c', '<C-n>', "<Cmd>call pum#map#insert_relative(+1, 'loop')<CR>")
+vim.keymap.set('c', '<C-p>', "<Cmd>call pum#map#insert_relative(-1, 'loop')<CR>")
+vim.keymap.set('c', '<C-y>', '<Cmd>call pum#map#confirm()<CR>')
 vim.fn['ddc#enable']()
 vim.fn['ddc#enable_cmdline_completion']()
 vim.fn['popup_preview#enable']()
