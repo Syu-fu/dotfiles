464baedd9bffb66c22aff4ceac49b11f3baa1546
Syu fu <syu.fu.dev@gmail.com>
2024-02-18 17:15:29 +0900

    feat: Feat Oil settings

--- home/.config/nvim/lua/plugins/oil.lua
+++ home/.config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,14 @@ return {
   {
     'stevearc/oil.nvim',
     cmd = 'Oil',
+    config = function()
+      require('oil').setup({
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+        },
+      })
+    end,
     dependencies = {
       { 'nvim-tree/nvim-web-devicons' },
     },
