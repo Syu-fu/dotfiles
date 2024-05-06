78fea7f684cf6f4c0c409328ca7ba9b13aa34e5d
Syu fu <syu.fu.dev@gmail.com>
2024-05-06 11:43:36 +0900

    fix: Fix zsh settings

--- dot_config/zsh/rc/alias.zsh
+++ dot_config/zsh/rc/alias.zsh
@@ -1,18 +1,9 @@
-abbr -S --quieter ..='cd ..'
-abbr -S --quieter p='cd $HOME/Documents/Projects'
-
-if command -v lsd > /dev/null; then
+if command -v lsd >/dev/null; then
 	alias ls="lsd"
 fi
 
-abbr -S --quieter nv='nvim'
-abbr -S --quieter g='git'
-alias cp='cp -ip'
+alias cp='cp -rip'
 alias mv='mv -i'
-alias rm='rm -i'
-
-abbr -S --quieter copypath='pwd | tr -d "\n" | xsel --clipboard --input'
-abbr -S --quieter lock='i3lock -i ~/Dropbox/wallpaper.jpg && sleep 1'
-abbr -S --quieter myip="ip -4 a show wlp0s20f3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
-
-abbr import-git-aliases -g --prefix --quieter "git "
+alias rm='rm -ri'
+alias grep="ggrep"
+alias sed="gsed"
--- dot_config/zsh/rc/hooks.zsh
+++ dot_config/zsh/rc/hooks.zsh
@@ -4,34 +4,54 @@ function cd-ls-hook() {
 }
 add-zsh-hook chpwd cd-ls-hook
 
-function ignore-history-hook() {
-	local line=${1%%$'\n'}
-	local cmd=${line%% *}
+export HIST_IGNORE_COMMAND="ls:cd:nvim:vim:less:which:whois:lazygit:xdg-open"
 
-	[[ ${#line} -ge 5
-		&& ${cmd} != 'ls'
-		&& ${cmd} != 'cd'
-		&& ${cmd} != 'nv'
-		&& ${cmd} != 'vim'
-		&& ${cmd} != 'less'
-		&& ${cmd} != 'which'
-		&& ${cmd} != 'whois'
-		&& ${cmd} != 'pacman'
-		&& ${cmd} != 'xdg-open'
-	]]
+function my_zshaddhistory() {
+	LASTHIST=$1
+
+	return 2
 }
-add-zsh-hook zshaddhistory ignore-history-hook
 
-function do-enter() {
-	if [ -n "$BUFFER" ]; then
-		zle accept-line
-		return 0
+# HIST_IGNORE_COMMANDのコマンドは記録しない
+# HISTFILEの最後にコマンドを記録
+# 重複する行はファイル上のものを削除してから最終行に追加
+# 空白から始まるコマンドは記録しない
+# 失敗したコマンドは記録しない
+# 直前に実行されたコマンドはLASTHISTに保存される
+function save_last_command_in_history_if_successful() {
+	# ignore empty command
+	if [[ ! -n ${LASTHIST:#[[:blank:]]*} ]]; then
+		return
 	fi
-	echo ""
-	ls -a
-	echo "\n"
-	zle reset-prompt
-	return 0
+
+	# ignore failed command
+	if [ ! $? -eq 0 ]; then
+		return
+	fi
+
+	# ignore space start command
+	if [[ ! ${LASTHIST} =~ ^\s ]]; then
+		local command_name=$(echo ${LASTHIST} | awk '{print $1}')
+		if [[ ! -n ${command_name} ]]; then
+			return
+		fi
+		# ignore HIST_IGNORE_COMMAND
+		for cmd in ${(s.:.)HIST_IGNORE_COMMAND}; do
+			if [[ "$command_name" == "$cmd" ]]; then
+				return
+			fi
+		done
+
+		# delete last history if it is duplicated
+		escaped_last_hist=$(echo "$LASTHIST" | sed 's/[\/&]/\\&/g')
+		if [ -f "${HISTFILE}" ]; then
+			sed -i "/^$escaped_last_hist$/d" "${HISTFILE}"
+		fi
+
+		echo "${LASTHIST%$'\n'}" >>"${HISTFILE}"
+	fi
 }
-zle -N do-enter
-bindkey '^m' do-enter
+
+add-zsh-hook precmd save_last_command_in_history_if_successful
+add-zsh-hook zshexit save_last_command_in_history_if_successful
+add-zsh-hook zshaddhistory my_zshaddhistory
