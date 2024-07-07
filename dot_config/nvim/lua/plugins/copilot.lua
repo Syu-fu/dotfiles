d4075b45cb110701013f856bc794fe5952cb6d2e
Syu fu <syu.fu.dev@gmail.com>
2024-07-07 16:19:22 +0900

    feat: Add copilot chat

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,29 +1,42 @@
 return {
   {
-    'zbirenbaum/copilot.lua',
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
     event = { 'InsertEnter', 'CmdlineEnter' },
-    config = function()
-      vim.defer_fn(function()
-        require('copilot').setup({
-          suggestion = {
-            auto_trigger = true,
-            keymap = {
-              accept = '<C-j>',
-              accept_word = false,
-              accept_line = false,
-              next = '<C-S-n>',
-              prev = '<C-S-p>',
-              dismiss = '<C-S-]>',
-            },
-            filetypes = {
-              gitcommit = true,
-            },
-          },
-        })
+    dependencies = {
+      {
+        'zbirenbaum/copilot.lua',
+        event = { 'InsertEnter', 'CmdlineEnter' },
+        config = function()
+          vim.defer_fn(function()
+            require('copilot').setup({
+              suggestion = {
+                auto_trigger = true,
+                keymap = {
+                  accept = '<C-j>',
+                  accept_word = false,
+                  accept_line = false,
+                  next = '<C-S-n>',
+                  prev = '<C-S-p>',
+                  dismiss = '<C-S-]>',
+                },
+                filetypes = {
+                  gitcommit = true,
+                },
+              },
+            })
 
-        vim.api.nvim_command('highlight link CopilotAnnotation LineNr')
-        vim.api.nvim_command('highlight link CopilotSuggestion LineNr')
-      end, 100)
-    end,
+            vim.api.nvim_command('highlight link CopilotAnnotation LineNr')
+            vim.api.nvim_command('highlight link CopilotSuggestion LineNr')
+          end, 100)
+        end,
+      },
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
