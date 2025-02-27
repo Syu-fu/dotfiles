adde6b12c72e000246456f8065ef1e0d2e8a69fc
Syu fu <syu.fu.dev@gmail.com>
2025-02-27 23:23:01 +0900

    fix: Fix Select commit type command

--- dot_config/nvim/after/ftplugin/gitcommit.lua
+++ dot_config/nvim/after/ftplugin/gitcommit.lua
@@ -1,19 +1,10 @@
---vim.cmd([[
--- function! s:select_type() abort
---   let line = substitute(getline('.'), '^;\s*', '', 'g')
---   let title = printf('%s: ', split(line, ' ')[0])
---   silent! normal! "_dip
---   silent! put! =title
---   silent! startinsert!
--- endfunction
--- nnoremap <buffer> <CR><CR> <Cmd>call <SID>select_type()<CR>
---]])
--- lua
 function Select_type()
-  local line = vim.fn.substitute(vim.fn.getline('.'), '^;s*', '', 'g')
+  local line = vim.fn.substitute(vim.fn.getline('.'), '^;\\s*', '', 'g')
   local title = string.format('%s: ', vim.fn.split(line, ' ')[1])
-  vim.cmd('silent! normal! "_dip')
-  vim.cmd('silent! put! =' .. title)
-  vim.cmd('silent! startinsert!')
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('"_dip', true, true, true), 'n', true)
+  vim.schedule(function()
+    vim.api.nvim_put({ title }, 'c', true, true)
+    vim.cmd('silent! startinsert!')
+  end)
 end
 vim.api.nvim_buf_set_keymap(0, 'n', '<CR><CR>', '<Cmd>lua Select_type()<CR>', { noremap = true, silent = true })
