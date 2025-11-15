11555ceca0230bb86286cdb141c5e334b32b13da
github-action-benchmark <github@users.noreply.github.com>
2025-11-15 08:27:19 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 065bcbe00071a40f0b1967ac27667a8c3bde8d9b

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1763195238982,
+  "repoUrl": "https://github.com/Syu-fu/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "syu.fu.dev@gmail.com",
+            "name": "Syu fu",
+            "username": "Syu-fu"
+          },
+          "committer": {
+            "email": "syu.fu.dev@gmail.com",
+            "name": "Syu fu",
+            "username": "Syu-fu"
+          },
+          "distinct": true,
+          "id": "065bcbe00071a40f0b1967ac27667a8c3bde8d9b",
+          "message": "fix(ci): Disable GPG signing in benchmark workflow\n\nDisable commit.gpgsign in CI environment to allow\nbenchmark-action to commit results to gh-pages branch\nwithout GPG key.",
+          "timestamp": "2025-11-15T17:26:36+09:00",
+          "tree_id": "c7efce49a091f43b21dbe4c8718234d245a952c7",
+          "url": "https://github.com/Syu-fu/dotfiles/commit/065bcbe00071a40f0b1967ac27667a8c3bde8d9b"
+        },
+        "date": 1763195238573,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh startup time (with plugins)",
+            "value": 29.28,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
